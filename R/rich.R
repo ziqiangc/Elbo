408003a9f1bd24854c27c655176f4c27daa5027b
Ziqiang Chen <ziqiangc@buffalo.edu>
2016-10-12 16:42:36 -0400

    Add rich data input (SummarizedExperiment)

---
+++ R/rich.R
@@ -0,0 +1,27 @@
+#' Rich data input
+#'
+#' Capture the semantic meaning of data that we present.
+#' For instance, separately recongnizing...
+#'
+#'
+#' @param pdata_file character(1) The path to the phenotype data file.
+#'
+#' @param exprs_file character(1) The path to the expression data file.
+#'
+#' @return A SummarizedExperiment() containing samples as colData(),
+#'      and expression values as assay().
+#'
+#' @export
+#'
+input_rich <- function(pdata_file, exprs_file) {
+    pdata <- read.csv(pdata_file, row.names=1, check.names=FALSE)
+    exprs <- read.csv(exprs_file, row.names=1, check.names=FALSE)
+
+    SummarizedExperiment(as.matrix(exprs), colData=pdata)
+
+    ## return a data.frame
+}
+
+
+
+
